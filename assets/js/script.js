0c894918b388f518fc3fed14ffed112db666a32f
hroddy <hannah.roddy@gmail.com>
2021-07-28 21:03:40 -0700

    rough draft of verdict screens

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,1 +1,30 @@
+//choose your pokemon button - script.js
 
+
+//pokemon choice event handlers - choose.js
+
+
+//google street view arena selection - google.js
+
+
+//begin fight button - choose.js
+
+//load user pokemon - choose.js
+
+//load enemy pokemon - choose.js
+
+
+//fight function - fight.js
+
+//battle log each turn - fight.js
+
+//event handlers for moves - fight.js
+
+//determine win or loss - fight.js
+
+//go to win/lose page - fight.js
+
+
+//save score - score.js
+
+//display highscore - score.js
